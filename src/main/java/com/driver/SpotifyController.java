c6bd88332a0861106c3bbfd1e683cd4ac939c9df
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-03-03 11:00:52 +0530

    Update SpotifyController.java

--- src/main/java/com/driver/SpotifyController.java
+++ src/main/java/com/driver/SpotifyController.java
@@ -1,16 +1,19 @@
 package com.driver;
 
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("spotify")
 public class SpotifyController {
-    private final SpotifyService spotifyService = new SpotifyService();
+
+    @Autowired
+    SpotifyService spotifyService;
 
     @PostMapping("/add-user")
-    public String createUser(@RequestParam(name = "name") String name, String mobile) {
+    public String createUser(@RequestParam(name = "name") String name, @RequestParam(name = "mobile") String mobile) {
         spotifyService.createUser(name, mobile);
         return "Success";
     }
@@ -22,37 +25,37 @@ public class SpotifyController {
     }
 
     @PostMapping("/add-album")
-    public String createAlbum(@RequestParam(name = "title") String title, String artistName) {
+    public String createAlbum(@RequestParam(name = "title") String title, @RequestParam(name = "artistName") String artistName) {
         spotifyService.createAlbum(title, artistName);
         return "Success";
     }
 
     @PostMapping("/add-song")
-    public String createSong(String title, String albumName, int length) throws Exception {
+    public String createSong(@RequestParam(name = "title") String title, @RequestParam(name = "albumName") String albumName, @RequestParam(name = "length") int length) throws Exception {
         spotifyService.createSong(title, albumName, length);
         return "Success";
     }
 
     @PostMapping("/add-playlist-on-length")
-    public String createPlaylistOnLength(String mobile, String title, int length) throws Exception {
+    public String createPlaylistOnLength(@RequestParam(name = "mobile") String mobile, @RequestParam(name = "title") String title, @RequestParam(name = "length") int length) throws Exception {
         spotifyService.createPlaylistOnLength(mobile, title, length);
         return "Success";
     }
 
     @PostMapping("/add-playlist-on-name")
-    public String createPlaylistOnName(String mobile, String title, @RequestBody List<String> songTitles) throws Exception {
+    public String createPlaylistOnName(@RequestParam(name = "mobile") String mobile, @RequestParam(name = "title") String title, @RequestParam(name = "songTitles") List<String> songTitles) throws Exception {
         spotifyService.createPlaylistOnName(mobile, title, songTitles);
         return "Success";
     }
 
     @PutMapping("/find-playlist")
-    public String findPlaylist(String mobile, String playlistTitle) throws Exception {
+    public String findPlaylist(@RequestParam(name = "mobile") String mobile, @RequestParam(name = "playlistTitle") String playlistTitle) throws Exception {
         spotifyService.findPlaylist(mobile, playlistTitle);
         return "Success";
     }
 
     @PutMapping("/like-song")
-    public String likeSong(String mobile, String songTitle) throws Exception {
+    public String likeSong(@RequestParam(name = "mobile") String mobile, @RequestParam(name = "songTitle") String songTitle) throws Exception {
         spotifyService.likeSong(mobile, songTitle);
         return "Success";
     }
